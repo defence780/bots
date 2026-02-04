60e5fc1b30b9ea59b8297825797a5f321b6b52e0
Saturo <60902599+saturoM@users.noreply.github.com>
2026-02-04 16:04:48 +0200

    hotfix

--- supabase/functions/atomic-transactions/add-awaiting-action-column.sql
+++
@@ -1,8 +0,0 @@
--- Додавання колонки для зберігання стану очікування дії користувача
--- Це дозволяє зберігати стан між викликами Edge Function
-
-ALTER TABLE IF EXISTS "analytics-users" 
-ADD COLUMN IF NOT EXISTS awaiting_action VARCHAR(50);
-
--- Коментар для документації
-COMMENT ON COLUMN "analytics-users".awaiting_action IS 'Статус очікування: null, report, lead, closer_report';
